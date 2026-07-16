3b73f2ad3cd1708d6e18fadff3561e8c7556f539
小弟调调 <398188662@qq.com>
2026-07-16 13:08:06 +0800

    Initial Commit

---
+++ DockLift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DockLift
+//
+//  Created by wong on 7/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DockLift/DockLiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockLiftApp.swift
+//  DockLift
+//
+//  Created by wong on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct DockLiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
